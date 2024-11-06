842f96e66a017250ccd67d87eeab01d9eafae895
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-05 23:00:44 -0500

    Tema: Documentación con SpringDoc | Swagger

--- demo/src/main/java/com/demoApiRestTemplate/demo/controller/PersonaNaturalController.java
+++ demo/src/main/java/com/demoApiRestTemplate/demo/controller/PersonaNaturalController.java
@@ -4,6 +4,8 @@ package com.demoApiRestTemplate.demo.controller;
 
 import com.demoApiRestTemplate.demo.entity.PersonaNaturalEntity;
 import com.demoApiRestTemplate.demo.service.PersonaNaturalService;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Nonnull;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,16 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/v1/personaNatural")
+@Tag(
+        name = "API GESTION DE PERSONAS NATURALES",
+        description = "Esta API contiene los EndPoints necesarios para la gestión de personas naturales " +
+                "esto incluye desde crear hasta eliminar una persona",
+        externalDocs = @ExternalDocumentation(
+                description = "Documentacion adicional de ApiRestTemplate",
+                url = "https://github.com/gfernandez-engineer/demoApiExtRestTemplate.git"
+        )
+
+)
 public class PersonaNaturalController {
 
     private final PersonaNaturalService personaNaturalService;
