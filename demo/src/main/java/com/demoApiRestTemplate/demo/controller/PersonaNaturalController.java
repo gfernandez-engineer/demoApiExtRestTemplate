05f271703d2581d94e6d4b9fee5b3dc663c0a21e
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-06 21:06:58 -0500

    Mejorando la documentacion de Swagger UI

--- demo/src/main/java/com/demoApiRestTemplate/demo/controller/PersonaNaturalController.java
+++ demo/src/main/java/com/demoApiRestTemplate/demo/controller/PersonaNaturalController.java
@@ -5,6 +5,12 @@ package com.demoApiRestTemplate.demo.controller;
 import com.demoApiRestTemplate.demo.entity.PersonaNaturalEntity;
 import com.demoApiRestTemplate.demo.service.PersonaNaturalService;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Nonnull;
 import org.antlr.v4.runtime.misc.NotNull;
@@ -38,7 +44,29 @@ public class PersonaNaturalController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Guardar una nueva persona natural",
+            description = "Crea una nueva persona natural y la almacena en la BD",
+            parameters = @Parameter(
+                    name = "dni",
+                    required = true,
+                    example = "12345678"
+            )
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Operacion Exitosa", content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PersonaNaturalEntity.class)),
+                    @Content(mediaType = "application/xml",
+                    schema = @Schema(implementation = PersonaNaturalEntity.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Error en el servidor"),
+            @ApiResponse(responseCode = "503", description = "INTERNAL ERROR FATAL")
+
+    }
+    )
     public ResponseEntity<PersonaNaturalEntity> guardarPersona(
+            @Parameter(description = "DNI de la persona a guardar", required = true)
             @RequestParam("dni") String dni) throws IOException {
         PersonaNaturalEntity personaNatural = personaNaturalService.guardar(dni);
         return new ResponseEntity<>(personaNatural, HttpStatus.CREATED);
